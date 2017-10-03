d46d41c38af4fdf2ac4434685b75279436f5631d
ken <ken@commerce21.co.jp>
2017-10-03 19:16:26 +0900

    optimize import

--- application/src/main/java/com/hyojinbae/sample/java9/spring/boot/controller/GreetingController.java
+++ application/src/main/java/com/hyojinbae/sample/java9/spring/boot/controller/GreetingController.java
@@ -2,13 +2,7 @@ package com.hyojinbae.sample.java9.spring.boot.controller;
 
 import com.hyojinbae.sample.java9.spring.boot.api.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpSession;
-import java.util.Date;
-import java.util.Optional;
 
 @RestController
 public class GreetingController {
