15c200f0ac0d9e7eb9abe4fd4bc32c68fe371078
Hyojin Bae <yaksaa21@gmail.com>
2017-10-04 11:45:43 +0900

    不要空白削除

--- application/src/main/java/com/hyojinbae/sample/java9/spring/boot/Java9SpringBootApplication.java
+++ application/src/main/java/com/hyojinbae/sample/java9/spring/boot/Java9SpringBootApplication.java
@@ -10,5 +10,4 @@ public class Java9SpringBootApplication {
 		SpringApplication.run(Java9SpringBootApplication.class, args);
 	}
 
-
 }
--- application/src/main/java/com/hyojinbae/sample/java9/spring/boot/controller/GreetingController.java
+++ application/src/main/java/com/hyojinbae/sample/java9/spring/boot/controller/GreetingController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-
     private GreetingService service;
     public GreetingController(GreetingService service) {
         this.service = service;
